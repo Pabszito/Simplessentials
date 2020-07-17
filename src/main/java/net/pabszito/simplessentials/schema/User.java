3f5575eed628ba82e64cd63afcf936bd3b19875e
Pabszito <37744843+Pabszito@users.noreply.github.com>
2020-07-17 16:37:18 -0300

    make user configuration serializable and add annotations

--- src/main/java/net/pabszito/simplessentials/schema/User.java
+++ src/main/java/net/pabszito/simplessentials/schema/User.java
@@ -1,11 +1,26 @@
 package net.pabszito.simplessentials.schema;
 
+import dev.morphia.annotations.Entity;
 import dev.morphia.annotations.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.SerializableAs;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-public class User implements Schema {
+@Entity("User") @SerializableAs("User") @NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class User implements Schema, ConfigurationSerializable {
 
     @Id @Getter
     private UUID id;
+
+    @Override
+    public Map<String, Object> serialize() {
+        Map<String, Object> serial = new HashMap<>();
+        serial.put("id", id);
+        return serial;
+    }
 }
